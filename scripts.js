426c374f98a53ffcd1695553191fa74b1ad0ff6b
Sina <sina@thewolpers.com>
2022-10-31 10:33:13 -0700

    Setup basic functions, html and css

---
+++ scripts.js
@@ -0,0 +1,26 @@
+let input1 = 10;
+let input2 = 2;
+let result = 0;
+
+function operate(operator) {
+	switch (operator) {
+		case 'add':
+			result = input1 + input2;
+			break;
+		case 'minus':
+			result = input1 - input2;
+			break;
+		case 'times':
+			result = input1 * input2;
+			break;
+		case 'divide':
+			result = (input1 / input2).toFixed(2);
+			break
+	}
+	console.log(result);
+}
+
+operate('add');
+operate('minus');
+operate('times');
+operate('divide');
