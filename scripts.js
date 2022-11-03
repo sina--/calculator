eb558381af76346841100dfb2bbcb965cc5596e4
Sina <sina@thewolpers.com>
2022-11-02 20:58:36 -0700

    Print last input on calculator display

--- scripts.js
+++ scripts.js
@@ -1,6 +1,21 @@
-let input1 = 10;
-let input2 = 2;
-let result = 0;
+const numButtons = document.querySelectorAll('[id=digit]');
+const opButtons = document.querySelectorAll('[id=operator]');
+const allOp = ['+', '-', '*', '/'];
+
+numButtons.forEach(numButton => numButton.addEventListener('click', init));
+opButtons.forEach(opButton => opButton.addEventListener('click', getOp));
+
+function init() {
+	let in1 = '';
+	in1 = this.textContent;
+	document.getElementById('display').textContent = in1;
+}
+
+function getOp() {
+	let in1 = '';
+	in1 = this.textContent;
+	console.log(in1);
+}
 
 function operate(operator) {
 	switch (operator) {
@@ -17,11 +32,5 @@ function operate(operator) {
 			result = (input1 / input2).toFixed(2);
 			break
 	}
-	console.log(result);
 	document.getElementById('display').textContent = result;
 }
-
-operate('add');
-operate('minus');
-operate('times');
-operate('divide');
