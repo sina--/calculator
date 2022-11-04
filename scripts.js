166ea97bda8913f5297579a5b36e24d7e51c0ba4
Sina <sina@thewolpers.com>
2022-11-03 20:24:51 -0700

    Change how the display functions

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 const numButtons = document.querySelectorAll('[id=digit]');
 const opButtons = document.querySelectorAll('[id=operator]');
-const display = document.getElementById('display');
+const displayPrevious = document.getElementById('displayPrevious');
+const displayOperator = document.getElementById('displayOperator');
+const displayLast = document.getElementById('displayLast');
+const displayExpression = document.getElementById('displayExpression');
 
 let previousIn = '';
 let lastIn = '';
@@ -10,23 +13,30 @@ let result = '';
 numButtons.forEach(numButton => numButton.addEventListener('click', buildBuffer));
 opButtons.forEach(opButton => opButton.addEventListener('click', buildBuffer));
 document.getElementById('calc').addEventListener('click', buildBuffer);
+document.getElementById('clear').addEventListener('click', buildBuffer);
 
 function buildBuffer() {
 	if(this.id === 'digit') {
 		lastIn += this.textContent;
-		display.textContent = lastIn;
+		displayLast.textContent = lastIn;
 	} else if(this.id === 'operator') {
+		operator = this.textContent;
 		previousIn = Number(lastIn);
 		lastIn = '';
-		operator = this.textContent;
-		display.textContent = operator;
 	} else if(this.id === 'calc') {
 		lastIn = Number(lastIn);
 		operate(operator);
-		previousIn = lastIn;
-		lastIn = result;
-		display.textContent = result;
+		let expression = `${previousIn} ${operator} ${lastIn} =`
+		displayExpression.textContent = expression;
+		lastIn = operate(operator);
+		previousIn = '';
+		operator = '';
+	} else if(this.id === 'clear') {
+		clear();
 	}
+	displayPrevious.textContent = previousIn;
+	displayOperator.textContent = operator;
+	displayLast.textContent = lastIn;
 }
 
 function operate(operator) {
@@ -45,4 +55,11 @@ function operate(operator) {
 			break
 	}
 	return result;
+}
+
+function clear() {
+	displayExpression.textContent = '';
+	previousIn = '';
+	operator = '';
+	lastIn = '';
 }
