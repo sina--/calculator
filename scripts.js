912901d1b0b07894c40b5a87ca564b94f98cce49
Sina <sina@thewolpers.com>
2022-11-09 10:16:07 -0800

    Add keyboard input functionality

--- scripts.js
+++ scripts.js
@@ -3,6 +3,8 @@ const displayPrevious = document.getElementById('displayPrevious');
 const displayOperator = document.getElementById('displayOperator');
 const displayLast = document.getElementById('displayLast');
 const displayExpression = document.getElementById('displayExpression');
+const digits = ['0','1','2','3','4','5','6','7','8','9','.'];
+const operators = ['+', '-', 'x', '^', '÷'];
 
 let previousIn = '';
 let lastIn = '';
@@ -10,7 +12,8 @@ let operator = '';
 let result = '';
 let bufferFull = false;
 
-buttons.forEach(button => button.addEventListener('click', buildBuffer));
+buttons.forEach(button => button.addEventListener('click', (event) => {buildBuffer(event.target)}));
+document.addEventListener('keypress', (event) => {onKeyPress(event.key)});
 buttons[2].addEventListener('click', clear)
 
 function clear() {
@@ -34,7 +37,37 @@ function checkBuffer() {
 	}
 }
 
-function buildBuffer() {
+function onKeyPress(key) {
+	console.log(key);
+	let tempData = {};
+	if(digits.includes(key)){
+		tempData = {
+			id: 'digit',
+			textContent: key,
+		};
+	} 
+	if(operators.includes(key)){
+		tempData = {
+			id: 'operator',
+			textContent: key,
+		};
+	}
+	if(key === 'Enter' || key === '=') {
+		tempData = {
+			id: 'calc',
+			textContent: key,
+		};
+	}
+	buildBuffer(tempData);
+}
+
+function buildBuffer(data = null) {
+	console.log(data);
+	if(data != null) {
+		this.id = data.id;
+		this.textContent = data.textContent;
+	}
+	console.log(this.id);
 	checkBuffer();
 	if(this.id === 'digit') {
 		lastIn += this.textContent;
